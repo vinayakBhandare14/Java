51b43259ddb16cc23ab8c28fc7cb9e48a2a65945
Vinayak Bhandare <vinayakbhandare2000@gmail.com>
2025-02-22 12:02:05 +0530

    Add files via upload

---
+++ ArrayList/ArrayListTypeSafe.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+class ArrayListTypeSafe 
+{
+	public static void main(String[] args) 
+	{
+		ArrayList<String> arrayString = new ArrayList();
+		arrayString.add("Vinayak");
+		arrayString.add("Luskesh");
+		arrayString.add("Sangli");
+		arrayString.add("11");
+		System.out.println("String Array List :: "+arrayString);
+	}
+}
+1
